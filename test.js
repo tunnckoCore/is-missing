9f737df5b015313254cea78f5fe98b9b28919a27
tunnckoCore <mameto_100@mail.bg>
2015-04-05 19:53:14 +0300

    move to `assertit`

--- test.js
+++ test.js
@@ -7,8 +7,7 @@
 
 'use strict';
 
-var test = require('testit');
-var assert = require('assert');
+var test = require('assertit');
 var isMissing = require('./index');
 
 test('is-missing:', function() {
@@ -17,8 +16,8 @@ test('is-missing:', function() {
       isMissing([1, 2, 3]);
     }
 
-    assert.throws(fixture, TypeError);
-    assert.throws(fixture, /to be string, but array given/);
+    test.throws(fixture, TypeError);
+    test.throws(fixture, /to be string, but array given/);
     done();
   });
 
@@ -27,39 +26,39 @@ test('is-missing:', function() {
       isMissing('foobar', 'callback');
     }
 
-    assert.throws(fixture, TypeError);
-    assert.throws(fixture, /to be function, but string given/);
+    test.throws(fixture, TypeError);
+    test.throws(fixture, /to be function, but string given/);
     done();
   });
 
   test('should return false if given `name` exist in npm registry', function(done) {
     isMissing('assert-kindof', function _cb(err, res) {
-      assert.strictEqual(err, null);
-      assert.strictEqual(res, false);
+      test.equal(err, null);
+      test.equal(res, false);
       done();
     });
   });
 
   test('should return true if given `name` not exist in npm registry', function(done) {
     isMissing('wombat-foo-bar-baz-qux-zero-four', function _cb(err, res) {
-      assert.strictEqual(err, null);
-      assert.strictEqual(res, true);
+      test.equal(err, null);
+      test.equal(res, true);
       done();
     });
   });
 
   test('should return false when repository already exists', function(done) {
     isMissing('tunnckoCore/assert-kindof', function _cb(err, res) {
-      assert.strictEqual(err, null);
-      assert.strictEqual(res, false);
+      test.equal(err, null);
+      test.equal(res, false);
       done();
     });
   });
 
   test('should return true when repository not exists', function(done) {
     isMissing('mochajs/kjerhkjghkjhhhhhhhhhhhh5kjdsvfsdf', function _cb(err, res) {
-      assert.strictEqual(err, null);
-      assert.strictEqual(res, true);
+      test.equal(err, null);
+      test.equal(res, true);
       done();
     });
   });
